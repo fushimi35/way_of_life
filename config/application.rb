eab0ac7974bfbcbead454be524432a035ed01adf
Karen Fushimi <lotus@forest.ocn.ne.jp>
2022-04-04 16:18:21 +0900

    課題1完了

--- config/application.rb
+++ config/application.rb
@@ -26,6 +26,12 @@ module RunteqNormal
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    config.generators do |g|
+      g.assets false    # <= css,javascriptファイルを作成しない
+      g.helper false    # <= helperファイルを作成しない
+      g.test_framework false  # <= テストファイルを作成しない
+      g.skip_routes true  # <= routes.rbを変更しない
+    end
 
     # Don't generate system test files.
     config.generators.system_tests = nil

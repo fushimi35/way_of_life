c664e30866a49ce18c0a683eaa8fa3c7979d20d8
Karen Fushimi <lotus@forest.ocn.ne.jp>
2022-04-14 11:22:01 +0900

    ログイン成功

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -25,15 +25,10 @@ class UsersController < ApplicationController
   # POST /users.json
   def create
     @user = User.new(user_params)
-
-    respond_to do |format|
-      if @user.save
-        format.html { redirect_to login_path, notice: 'User was successfully created.' }
-        format.json { render :show, status: :created, location: login_path }
-      else
-        format.html { render :new }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
+    if @user.save
+      redirect_to login_url, notice: 'User was successfully created.'
+    else
+      render :new
     end
   end
 

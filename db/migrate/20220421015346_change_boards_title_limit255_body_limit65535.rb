7b67b2e400b0e1490b13a2f09e024fd6b6926213
Karen Fushimi <lotus@forest.ocn.ne.jp>
2022-04-21 11:50:39 +0900

    モデル追加、制限・バリデーション追加済み

---
+++ db/migrate/20220421015346_change_boards_title_limit255_body_limit65535.rb
@@ -0,0 +1,10 @@
+class ChangeBoardsTitleLimit255BodyLimit65535 < ActiveRecord::Migration[5.2]
+  def up
+    change_column :boards, :title, :string, limit: 255
+    change_column :boards, :body, :text, limit: 65535
+  end
+  def down
+    change_column :boards, :title, :string
+    change_column :boards, :body, :text
+  end
+end

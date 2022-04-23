7acd3885d3041102350505887ecfb884c6cc9d19
Karen Fushimi <lotus@forest.ocn.ne.jp>
2022-04-23 13:28:30 +0900

    電源消す直前

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -2,4 +2,22 @@ class BoardsController < ApplicationController
   def index
     @boards = Board.all.includes(:user).order(created_at: :desc)
   end
+  def new
+    @board = current_user.boards.new()
+  end
+  def create
+    @board = current_user.boards.new(board_params)
+    if @board.save!
+      redirect_to boards_path, success: t('.success')
+    else
+      flash.now[:danger] = t('.fail')
+      render :new
+    end
+
+  end
+
+  private
+  def board_params
+    params.require(:board).permit(:title, :body, :user_id)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'static_pages#top'
   resources :users, only: %i[new create]
-  resources :boards, only: %i[index]
+  resources :boards, only: %i[index new create]
 
   get 'login' => 'user_sessions#new', :as => :login
   post 'login' => 'user_sessions#create'

09f910ca1242b024274859ecfa23e84d88d09649
Karen Fushimi <lotus@forest.ocn.ne.jp>
2022-04-29 16:49:53 +0900

    課題11解答チェック後

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -4,11 +4,9 @@ class BoardsController < ApplicationController
   end
   def new
     @board = current_user.boards.new()
-    binding.pry
   end
   def create
     @board = current_user.boards.new(board_params)
-    binding.pry
     if @board.save
       redirect_to boards_path, success: t('defaults.message.created', item: Board.model_name.human)
     else

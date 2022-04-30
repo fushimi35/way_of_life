6a94fb07696a10ac2eda7db5270b74f99afa9720
Karen Fushimi <lotus@forest.ocn.ne.jp>
2022-04-30 17:24:46 +0900

    詳細ページパーシャル化成功

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -13,7 +13,9 @@ class BoardsController < ApplicationController
       flash.now[:danger] = t('defaults.message.not_created', item: Board.model_name.human)
       render :new
     end
-
+  end
+  def show
+    @board = Board.find(params[:id])
   end
 
   private

a2579c50e46abba036591d998f91345cc2ea0873
Karen Fushimi <lotus@forest.ocn.ne.jp>
2022-05-03 08:30:27 +0900

    コメント作成成功

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,16 @@
 class CommentsController < ApplicationController
+  def index
+  end
+  def create
+    @board = Board.find(params[:board_id])
+    binding.pry
+    @comment = @board.comments.new(comment_params)
+    @comment.user_id = current_user.id
+    if @comment.save
+      redirect_to board_path(@board)
+    end
+  end
+  def comment_params
+    params.require(:comment).permit(:body)
+  end
 end

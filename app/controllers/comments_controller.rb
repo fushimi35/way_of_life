1f77b39af18fe341546cc93f8f04cdcc359fcb97
Karen Fushimi <lotus@forest.ocn.ne.jp>
2022-05-03 17:16:52 +0900

    課題12自力実装

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,13 +1,14 @@
 class CommentsController < ApplicationController
-  def index
-  end
   def create
     @board = Board.find(params[:board_id])
-    binding.pry
     @comment = @board.comments.new(comment_params)
     @comment.user_id = current_user.id
     if @comment.save
-      redirect_to board_path(@board)
+      redirect_to board_path(@board), success: t('defaults.message.created', item: Comment.model_name.human)
+    else
+      @comments = @board.comments.all
+      flash.now[:danger] = t('defaults.message.not_created', item: Comment.model_name.human) 
+      render 'boards/show' 
     end
   end
   def comment_params

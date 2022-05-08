9e61f4e02fd751777d07a2803866435625a71e62
Karen Fushimi <lotus@forest.ocn.ne.jp>
2022-05-08 15:36:54 +0900

    画像サイズ調整

--- app/uploaders/board_image_uploader.rb
+++ app/uploaders/board_image_uploader.rb
@@ -1,7 +1,8 @@
 class BoardImageUploader < CarrierWave::Uploader::Base
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
-  # include CarrierWave::MiniMagick
+  include CarrierWave::MiniMagick
+  process resize_to_fill: [nil, 200]
 
   # Choose what kind of storage to use for this uploader:
   storage :file

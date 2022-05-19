70be7738404f4415926348d0523a428170b27d9b
Karen Fushimi <lotus@forest.ocn.ne.jp>
2022-05-19 19:12:26 +0900

    destroy修正

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -6,8 +6,8 @@ class BookmarksController < ApplicationController
   end
 
   def destroy
-    bookmark = current_user.bookmarks.find_by(board_id: params[:id])
-    bookmark.destroy!
-    redirect_to request.referer, success: t('.success')
+    board = current_user.bookmarks.find(params[:id]).board
+    current_user.unbookmark(board)
+    redirect_back fallback_location: root_path, success: t('.success')
   end
 end

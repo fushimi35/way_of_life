3ab4a0a8beaef9ce2bf16fb9db5075c933a97598
Karen Fushimi <lotus@forest.ocn.ne.jp>
2022-06-20 15:17:58 +0900

    課題22解答チェック後

--- app/controllers/admin/user_sessions_controller.rb
+++ app/controllers/admin/user_sessions_controller.rb
@@ -1,13 +1,14 @@
 class Admin::UserSessionsController < Admin::BaseController
-  skip_before_action :require_login
+  skip_before_action :require_login, only: %i[new create]
+  skip_before_action :check_admin, only: %i[new create]
   layout 'admin_login'
+
   def new; end
 
   def create
     @user = login(params[:email],params[:password])
     if @user
-      path = @user.role == 'admin' ? admin_root_path : boards_path
-      redirect_back_or_to(path, success: t('.success'))
+      redirect_to admin_root_path, success: t('.success')
     else
       flash.now[:danger] = t('.fail')
       render action: 'new'
